202ed74db40fad49af329bd3c1f12aeb98e1179c
Aleksandr Shakhov <alex1097@mail.ru>
2022-01-04 15:01:28 +0300

    Препроцессинг сырой строки - нижний регистр - термы адреса приведены к нормальной форме

--- src/main/java/AddressMatch.java
+++ src/main/java/AddressMatch.java
@@ -1,7 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class AddressMatch {
@@ -12,7 +18,31 @@ public class AddressMatch {
     }
 
     private static String preprocess(String raw){
-        // нижний регистр
-        return raw.toLowerCase(Locale.ROOT);
+        // приводим к нижнему регистру
+        raw = raw.toLowerCase(Locale.ROOT);
+
+        //TODO вынести в отдельный класс
+        //приводим к единому формату терм
+        // набор преобразований
+        Map<String,String> terms = new HashMap<>();
+        terms.put("область", "обл");
+        terms.put("город", "г");
+        terms.put("улица", "у");
+        terms.put("дом", "д");
+        terms.put("строение", "стр");
+        terms.put("корпус", "к");
+        terms.put("квартира", "кв");
+
+        String patternString = "%(" + StringUtils.join(terms.keySet()) + ")%";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(raw);
+
+        StringBuilder sb = new StringBuilder();
+        while(matcher.find()) {
+            matcher.appendReplacement(sb, terms.get(matcher.group(1)));
+        }
+        matcher.appendTail(sb);
+
+        return sb.toString();
     }
 }
